3cf669254bf71341b275265aab95db4f5b7e2551
Oscar <oscar.nordstrom2000@gmail.com>
2023-09-25 16:22:21 +0200

    🚧feat: contact button colors

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -7,8 +7,8 @@ import ImageBig from "./ImageBig.vue";
   <dvi class="flex flex-col justify-center items-center gap-60 w-[100vw] h-[90vh] bg-cust-blue-1">
     <div class="flex items-center justify-center gap-36 text-center">
       <div class="flex flex-col items-center gap-32">
-        <h1 class="text-8xl">Hello I'm <span class="text-cust-blue-2">Oscar</span> <br> Game and Fullstack <br> Developer</h1>
-        <button class="btn w-[10vw]">Contact</button>
+        <h1 class="text-8xl text-cust-blue-5">Hello I'm <span class="text-cust-blue-2">Oscar</span> <br> Game and Fullstack <br> Developer</h1>
+        <button class="btn bg-cust-blue-2 w-[10vw] hover:drop-shadow-xl hover:bg-cust-blue-2 text-white hover:bg-cust-blue-1 hover:text-cust-blue-5">Contact</button>
       </div>
       <ImageBig></ImageBig>
     </div>
