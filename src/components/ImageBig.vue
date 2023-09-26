008b443bdcba25a886f930f14a529dd0cd71f1be
Oscar <oscar.nordstrom2000@gmail.com>
2023-09-26 10:11:12 +0200

    ✨feat: responsive design

--- src/components/ImageBig.vue
+++ src/components/ImageBig.vue
@@ -3,12 +3,7 @@
 </script>
 
 <template>
-
-  <div className="relative w-[20vw] h-[35vh]">
-    <img src="https://avatars.githubusercontent.com/u/35327167?s=400&u=875a2d43e46e3552b6d5df5a89690f0f3870e372&v=4" class="absolute w-[20vw] rounded-full z-[1]">
-    <div className="absolute top-[20px] left-[20px] w-[20vw] h-[20vw] border-[5px] border-solid rounded-2xl z-[0]"/>
-    <div className="absolute top-[-20px] left-[-20px] w-[20vw] h-[20vw] border-[8px] border-solid rounded-2xl z-[0]"/>
-  </div>
+    <img src="https://avatars.githubusercontent.com/u/35327167?s=400&u=875a2d43e46e3552b6d5df5a89690f0f3870e372&v=4" class="w-[20vw] rounded-full z-[1] ">
 </template>
 
 <style scoped>
--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -5,10 +5,10 @@ import ButtonDefault from "./ButtonDefault.vue";
 </script>
 
 <template>
-  <dvi class="flex flex-col justify-center items-center gap-60 w-[100vw] h-[90vh]">
-    <div class="flex items-center justify-center gap-36 text-center">
-      <div class="flex flex-col items-center gap-32">
-        <h1 class="text-8xl">Hello I'm <span class="text-cust-blue-2">Oscar</span> <br> Game and Fullstack <br> Developer</h1>
+  <dvi class="flex justify-center w-[100vw] h-[90vh]">
+    <div class="flex flex-col-reverse lg:flex-row items-center justify-center gap-10 lg:gap-32 text-center">
+      <div class="flex flex-col items-center gap-10 lg:gap-32">
+        <h1 class="text-[10vw] md:text-[7vw] lg:text-[5vw]">Hello I'm <span class="text-cust-blue-2">Oscar</span> <br> Game and Fullstack <br> Developer</h1>
         <ButtonDefault text="Contact"></ButtonDefault>
       </div>
       <ImageBig></ImageBig>
