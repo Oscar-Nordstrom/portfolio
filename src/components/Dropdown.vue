6b0712b952019084409f4f58708167b16cd7f59b
Oscar <oscar.nordstrom2000@gmail.com>
2023-09-26 13:44:18 +0200

    ✨feat: dropdown responsive

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -16,7 +16,7 @@ defineProps<{ props: prop[]}>()
   <div class="dropdown dropdown-hover dropdown-end">
     <label tabindex="0" class="btn bg-transparent border-0"><v-icon name="bi-list" scale="1.5"/></label>
     <div tabindex="0" class="dropdown-content menu">
-      <div class="flex flex-col justify-center items-center gap-2">
+      <div class="flex flex-col lg:flex-row justify-center items-center gap-2">
         <ButtonDefault v-for="prop in props" :text="prop.label" :linkTo="prop.route"></ButtonDefault>
       </div>
     </div>
