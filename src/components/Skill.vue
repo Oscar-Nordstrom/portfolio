99e177525e4053dcc4c69746da067aeae2688000
Oscar <oscar.nordstrom2000@gmail.com>
2023-09-26 15:52:37 +0200

    ✨feat: added some skills

---
+++ src/components/Skill.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{ icon:string, text:string}>()
+
+function toKebab(text): string{
+  return text
+      .replace(/([a-z])([A-Z])/g, "$1-$2")
+      .replace(/[\s_]+/g, '-')
+      .toLowerCase();
+}
+
+</script>
+
+<template>
+  <div class="flex gap-2">
+    <v-icon :name="toKebab(icon)"></v-icon>
+    <h3>{{text}}</h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,96 @@ import Home from "./Pages/Home.vue";
 import Contact from "./Pages/Contact.vue";
 import NotFound from "./Pages/NotFound.vue";
 
+
 import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { BiList, BiGithub, BiLinkedin, BiXLg, FaPhoneAlt, MdAlternateemail, MdDrivefilerenameoutline} from "oh-vue-icons/icons";
-addIcons(BiList, BiGithub, BiLinkedin, BiXLg, FaPhoneAlt, MdAlternateemail, MdDrivefilerenameoutline);
+
+
+import {
+    BiList,
+    BiGithub,
+    BiLinkedin,
+    BiXLg,
+    FaPhoneAlt,
+    MdAlternateemail,
+    MdDrivefilerenameoutline,
+    ViFileTypeCsharp2,
+    ViFileTypeCpp3,
+    ViFileTypeReactjs,
+    ViFileTypeAngular,
+    CoNextJs,
+    ViFileTypeVue,
+    ViFileTypeTypescriptOfficial,
+    CoJavascript,
+    CoDotNet,
+    ViFileTypeHtml,
+    ViFileTypeCss,
+    ViFileTypeTailwind,
+    ViFileTypeNode2,
+    ViFileTypeLua,
+    RiTestTubeFill,
+    CoDotNet,
+    AiStackoverflowSquare,
+    IoGameControllerSharp,
+    MdDataobjectTwotone,
+    ViFileTypePlsql,
+    ViFileTypePlsql,
+    ViFileTypePlsql,
+    ViFileTypeAzure,
+    BiKeyFill,
+    ViFileTypeDocker2,
+    RiTeamFill,
+    RiTestTubeFill,
+    GiArtificialIntelligence,
+    RiTeamFill,
+    CoUnity,
+    MdGames,
+    ViFileTypeArduino,
+    BiGit
+
+} from "oh-vue-icons/icons";
+import Skills from "./Pages/Skills.vue";
+addIcons(
+    BiList,
+    BiGithub,
+    BiLinkedin,
+    BiXLg,
+    FaPhoneAlt,
+    MdAlternateemail,
+    MdDrivefilerenameoutline,
+    ViFileTypeCsharp2,
+    ViFileTypeCpp3,
+    ViFileTypeReactjs,
+    ViFileTypeAngular,
+    CoNextJs,
+    ViFileTypeVue,
+    ViFileTypeTypescriptOfficial,
+    CoJavascript,
+    CoDotNet,
+    ViFileTypeHtml,
+    ViFileTypeCss,
+    ViFileTypeTailwind,
+    ViFileTypeNode2,
+    ViFileTypeLua,
+    RiTestTubeFill,
+    CoDotNet,
+    AiStackoverflowSquare,
+    IoGameControllerSharp,
+    MdDataobjectTwotone,
+    ViFileTypePlsql,
+    ViFileTypePlsql,
+    ViFileTypePlsql,
+    ViFileTypeAzure,
+    BiKeyFill,
+    ViFileTypeDocker2,
+    RiTeamFill,
+    RiTestTubeFill,
+    GiArtificialIntelligence,
+    RiTeamFill,
+    CoUnity,
+    MdGames,
+    ViFileTypeArduino,
+    BiGit
+);
 
 
 
@@ -17,6 +104,7 @@ const router = createRouter({
     routes: [
         { path: '/', name:'Home', component: Home },
         { path: '/contact', name:'Contact', component: Contact },
+        { path: '/skills', name:'Skills', component: Skills },
         { path: '/:pathMatch(.*)*', name:'NotFound', component: NotFound }
     ]
 })
