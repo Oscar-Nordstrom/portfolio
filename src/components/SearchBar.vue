bf1397d0abf2bae6bd6406608ed25d81f1d85428
Oscar <oscar.nordstrom2000@gmail.com>
2023-09-27 14:19:55 +0200

    🐛fix: update skills when clear search

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,16 +5,17 @@ defineProps<{ onChange: Function}>()
 function update(event: any, fnChange: Function){
   fnChange(event.target.value);
 }
-function clear(){
+function clear(fnChange: Function){
   let input = (<HTMLInputElement>document.getElementById("searchInput"));
   input.value = "";
+  fnChange(input.value);
 }
 </script>
 
 <template>
     <div class="flex justify-end items-center w-[70vw] lg:w-[30vw] h-[5vh] border-4 rounded-full">
       <div class="px-5 ">
-        <v-icon class="cursor-pointer" @click="clear" name="bi-x-lg"></v-icon>
+        <v-icon class="cursor-pointer" @click="clear(onChange)" name="bi-x-lg"></v-icon>
       </div>
       <input @input="update($event, onChange)" type="text" id="searchInput" class="w-full h-full border-0 bg-transparent focus:border-0 focus:outline-0 focus:outline-none"/>
       <div class="px-5">
