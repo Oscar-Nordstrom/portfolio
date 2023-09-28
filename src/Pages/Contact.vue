19661c0c3b5e5f1510b7be444896599b0a120991
Oscar <oscar.nordstrom2000@gmail.com>
2023-09-28 08:06:10 +0200

    🐛fix: About mobile layout

--- src/Pages/Contact.vue
+++ src/Pages/Contact.vue
@@ -6,7 +6,7 @@ import ImageBig from "../components/ImageBig.vue";
   <div class="flex justify-center w-[100vw] h-[88vh]">
     <div class="flex flex-col lg:flex-row items-center justify-center gap-10 lg:gap-32 text-center">
       <ImageBig></ImageBig>
-      <div class="flex flex-col  items-center gap-10">
+      <div class="flex flex-col  items-start gap-10">
         <h1 class="text-[10vw] md:text-[7vw] lg:text-[5vw]">Contact <span class="text-cust-blue-2">Me</span></h1>
         <div class="flex flex-col gap-3">
           <div class="flex gap-3">
