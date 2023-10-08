c6801d028f397efc3624a31bbb184cd4f772d2d7
Oscar <oscar.nordstrom2000@gmail.com>
2023-10-08 20:30:24 +0200

    ✨feat: Add image of myself

--- src/components/ImageBig.vue
+++ src/components/ImageBig.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <img src="https://avatars.githubusercontent.com/u/35327167?s=400&u=875a2d43e46e3552b6d5df5a89690f0f3870e372&v=4" class="w-[20vw] rounded-full z-[1] ">
+    <img src="/img.jpg" class="w-[20vw] rounded-full z-[1] ">
 </template>
 
 <style scoped>
